fa281c93562361fad58d3f13540138ba9db838b1
Loki Coyote <lokkju@gmail.com>
2019-09-13 10:47:11 -0400

    fixing workflows

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,6 +19,4 @@ echo "${INPUT_SETTINGS_SBT}" > ./project/github_action_sbt_settings.sbt
 set -x
 /usr/bin/sbt "${@}"
 
-rm ./project/github_action_sbt_settings.sbt
-
 #echo ::set-output name=time::$time
