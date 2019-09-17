6942950021cc28a32be0dce10f949030de3128fc
Loki Coyote <lokkju@gmail.com>
2019-09-17 14:18:45 -0700

    adding matrix branch generator

---
+++ branch_matrix_generator.sh
@@ -0,0 +1,5 @@
+JAVA_VERSIONS = ["8" "11" "14"]
+SBT_VERSIONS = ["0.13.18" "1.0.4" "1.1.6" "1.2.8" "1.3.0"]
+SCALA_VERSION = ["2.13.0","2.12.10","2.11.12","2.10.7"]
+
+#sed 's/\${SCALA_VERSION:-2.13.0}/\${SCALA_VERSION:-2.13.0}/g' Dockerfile
