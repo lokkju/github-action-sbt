fc86764735b11b19783afee94ad2b8e862014860
Loki Coyote <lokkju@gmail.com>
2019-09-18 05:12:17 -0700

    adding tags

--- branch_matrix_generator.sh
+++ branch_matrix_generator.sh
@@ -6,6 +6,8 @@ SCALA_VERSIONS=("2.13.0" "2.12.10" "2.11.12" "2.10.7")
 
 # echo deleting exiting branches
 # git branch -r |  grep "^  origin/" | sed "s|^  origin/|:|" | grep -v "^:HEAD" | grep -v "^:master$" | xargs git push origin
+# echo deleting existing remote tags
+# git push origin :refs/tags/<tagname>
 
 for JAVA_VERSION in ${JAVA_VERSIONS[@]}
 do
@@ -18,7 +20,7 @@ do
       sed -i "s/\${SBT_VERSION:.*}/\${SBT_VERSION:-$SBT_VERSION}/g" Dockerfile
       sed -i "s/\${SCALA_VERSION:.*}/\${SCALA_VERSION:-$SCALA_VERSION}/g" Dockerfile
       git commit -a -m "Updating branch to for new version"
-      git tag ${JAVA_VERSION}-${SBT_VERSION}-${SCALA_VERSION}
+      git tag -f ${JAVA_VERSION}-${SBT_VERSION}-${SCALA_VERSION}
       git checkout master
     done
   done
